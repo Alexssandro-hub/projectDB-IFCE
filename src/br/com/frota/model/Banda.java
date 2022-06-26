e80b2fa8f41603d80074773c13ce911823f3ed9b
Alexssandro Rodrigues Tabosa <alexssandro.rodrigues.tabosa08@aluno.ifce.edu.br>
2022-06-25 21:16:15 -0300

    Create entities and DAO

---
+++ src/br/com/frota/model/Banda.java
@@ -0,0 +1,35 @@
+package br.com.frota.model;
+
+public class Banda extends GenericModel {
+    private String descricao;
+    private String lado;
+
+    public Banda(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Banda(Integer id, String descricao, String lado) {
+        this.descricao = descricao;
+        this.lado = lado;
+        super.setId(id);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public String getLado(){ return lado; }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void setLado(String lado) { this.lado = lado; }
+
+    @Override
+    public String toString() {
+        return "Banda {" +
+                "id='" + this.getId() + "\'" +
+                "descricao='" + descricao + "\'" +
+                "lado='" + lado + "\'" +
+                '}';
+    }
+}
---
+++ src/resources/inserts.sql
@@ -0,0 +1,33 @@
+--MARCA
+INSERT INTO marca (descricao) VALUES ('Toyota');
+
+--MARCA PNEU
+INSERT INTO marca_pneu (descricao) VALUES ('goodyear');
+INSERT INTO marca_pneu (descricao) VALUES ('pirelli');
+INSERT INTO marca_pneu (descricao) VALUES ('michelin');
+INSERT INTO marca_pneu (descricao) VALUES ('toyo');
+INSERT INTO marca_pneu (descricao) VALUES ('dayton');
+INSERT INTO marca_pneu (descricao) VALUES ('multi-mile');
+INSERT INTO marca_pneu (descricao) VALUES ('kenda');
+INSERT INTO marca_pneu (descricao) VALUES ('firestone');
+INSERT INTO marca_pneu (descricao) VALUES ('gt radial');
+INSERT INTO marca_pneu (descricao) VALUES ('marshal');
+INSERT INTO marca_pneu (descricao) VALUES ('yokohama');
+
+-- TIPO EIXO
+INSERT INTO tipo_eixo (descricao) VALUES ('redondo');
+INSERT INTO tipo_eixo (descricao) VALUES ('descricaoParaTeste');
+INSERT INTO tipo_eixo (descricao) VALUES ('veio com defeito');
+INSERT INTO tipo_eixo (descricao) VALUES ('eixo novinho');
+INSERT INTO tipo_eixo (descricao) VALUES ('eixo desgastado');
+
+-- BANDA
+INSERT INTO banda (descricao, lado) VALUES ('teste, direito');
+INSERT INTO banda (descricao, lado) VALUES ('testeLado, esquerdo');
+INSERT INTO banda (descricao, lado) VALUES ('Banda do pneu OK, direito');
+INSERT INTO banda (descricao, lado) VALUES ('Banda desgastada, esquerdo');
+INSERT INTO banda (descricao, lado) VALUES ('Banda horrível, direito');
+
+
+
+
