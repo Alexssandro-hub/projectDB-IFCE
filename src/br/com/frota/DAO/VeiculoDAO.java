e72670638b4ad3f0534c37414953f2dc07c35862
Alexssandro Rodrigues Tabosa <alexssandro.rodrigues.tabosa08@aluno.ifce.edu.br>
2022-06-26 21:36:42 -0300

    Data access of entities

--- src/br/com/frota/DAO/VeiculoDAO.java
+++ src/br/com/frota/DAO/VeiculoDAO.java
@@ -1,6 +1,6 @@
 package br.com.frota.DAO;
 
-import br.com.frota.model.Banda;
+import br.com.frota.model.Veiculo;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,14 +8,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BandaDAO extends ConexaoDB {
+public class VeiculoDAO extends ConexaoDB {
 
-    private static final String INSERT_BANDA_SQL = "INSERT INTO banda (descricao, lado) VALUES (?, ?);";
-    private static final String SELECT_BANDA_BY_ID = "SELECT id, descricao, lado FROM banda WHERE id = ?";
-    private static final String SELECT_ALL_BANDA = "SELECT * FROM banda;";
-    private static final String DELETE_BANDA_SQL = "DELETE FROM banda WHERE id = ?;";
-    private static final String UPDATE_BANDA_SQL = "UPDATE banda SET descricao = ?, lado = ? WHERE id = ?;";
-    private static final String TOTAL = "SELECT count(1) FROM banda;";
+    private static final String INSERT_VEICULO_SQL = "INSERT INTO veiculo (descricao, id_marca) VALUES (?, ?);";
+    private static final String SELECT_VEICULO_BY_ID = "SELECT id, descricao, id_marca FROM veiculo WHERE id = ?";
+    private static final String SELECT_ALL_VEICULO = "SELECT * FROM veiculo;";
+    private static final String DELETE_VEICULO_SQL = "DELETE FROM veiculo WHERE id = ?;";
+    private static final String UPDATE_VEICULO_SQL = "UPDATE veiculo SET descricao = ?, id_marca = ? WHERE id = ?;";
+    private static final String TOTAL = "SELECT count(1) FROM veiculo;";
 
     public Integer count() {
         Integer count = 0;
@@ -33,9 +33,9 @@ public class BandaDAO extends ConexaoDB {
         return count;
     }
 
-    public void insertBanda(Banda entidade) {
-        try (PreparedStatement preparedStatement = prapararSQL(INSERT_BANDA_SQL)) {
-            preparedStatement.setString(1, entidade.getDescricao(), entidade.getLado());
+    public void insertVeiculo(Veiculo entidade) {
+        try (PreparedStatement preparedStatement = prapararSQL(INSERT_VEICULO_SQL)) {
+            preparedStatement.setString(1, entidade.getDescricao(), entidade.getIdMarca());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             printSQLException(e);
@@ -44,16 +44,16 @@ public class BandaDAO extends ConexaoDB {
         }
     }
 
-    public Banda selectBanda(int id) {
-        Banda entidade = null;
-        try (PreparedStatement preparedStatement = prapararSQL(SELECT_BANDA_BY_ID)) {
+    public Veiculo selectVeiculo(int id) {
+        Veiculo entidade = null;
+        try (PreparedStatement preparedStatement = prapararSQL(SELECT_VEICULO_BY_ID)) {
             preparedStatement.setInt(1, id);
             ResultSet rs = preparedStatement.executeQuery();
 
             while (rs.next()) {
                 String descricao = rs.getString("descricao");
-                String lado = rs.getString("lado");
-                entidade = new Banda(id, descricao, lado);
+                Integer id_marca = rs.getInt("id_marca");
+                entidade = new Veiculo(id, descricao, id_marca);
             }
         } catch (SQLException e) {
             printSQLException(e);
@@ -63,16 +63,16 @@ public class BandaDAO extends ConexaoDB {
         return entidade;
     }
 
-    public List<Banda> selectAllBanda() {
-        List<Banda> entidades = new ArrayList<>();
-        try (PreparedStatement preparedStatement = prapararSQL(SELECT_ALL_BANDA)) {
+    public List<Veiculo> selectAllVeiculo() {
+        List<Veiculo> entidades = new ArrayList<>();
+        try (PreparedStatement preparedStatement = prapararSQL(SELECT_ALL_VEICULO)) {
             ResultSet rs = preparedStatement.executeQuery();
 
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String descricao = rs.getString("descricao");
-                String lado = rs.getString("lado");
-                entidades.add(new Banda(id, descricao, lado));
+                Integer id_marca = rs.getInt("id_marca");
+                entidades.add(new Veiculo(id, descricao, id_marca));
             }
         } catch (SQLException e) {
             printSQLException(e);
@@ -82,8 +82,8 @@ public class BandaDAO extends ConexaoDB {
         return entidades;
     }
 
-    public boolean deleteBanda(int id) throws SQLException {
-        try (PreparedStatement statement = prapararSQL(DELETE_BANDA_SQL)) {
+    public boolean deleteVeiculo(int id) throws SQLException {
+        try (PreparedStatement statement = prapararSQL(DELETE_VEICULO_SQL)) {
             statement.setInt(1, id);
             return statement.executeUpdate() > 0;
         } catch (ClassNotFoundException e) {
@@ -91,11 +91,10 @@ public class BandaDAO extends ConexaoDB {
         }
     }
 
-    public boolean updateBanda(Banda entidade) throws SQLException {
-        try (PreparedStatement statement = prapararSQL(UPDATE_BANDA_SQL)) {
-            statement.setString(1, entidade.getDescricao();
-            statement.setString(2, entidade.getLado());
-            statement.setInt(3, entidade.getId());
+    public boolean updateVeiculo(Veiculo entidade) throws SQLException {
+        try (PreparedStatement statement = prapararSQL(UPDATE_VEICULO_SQL)) {
+            statement.setString(1, entidade.getDescricao());
+            statement.setInt(2, entidade.getId());
 
             return statement.executeUpdate() > 0;
         } catch (ClassNotFoundException e) {
